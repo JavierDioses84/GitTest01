c13fb815ff159599d28faf20791c084b5d4f37f6
JavierDioses84 <javier.diosesz@gmail.com>
2024-08-02 20:10:31 -0500

    Cambios antes de mezclar las ramas

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ print("primera linea")
 import tratamientoDx as tx
 
 print(tx.eliminarDx())
+
+print("por agregar cambios en la rama exploracion")
+
+print("por agregar cambios en la rama tratamiento")
