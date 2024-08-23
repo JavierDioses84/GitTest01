cfe21dd1ceb3adc86992efe97f8f0cc10765f644
Javier <javier.diosesz@gmail.com>
2024-08-23 17:13:29 -0500

    actualizando main.py

--- main.py
+++ main.py
@@ -5,11 +5,3 @@ import exploracionDx as ed
 print(ed.correlacionPares())
 
 print(ed.correlacionImpares())
-
-print("Linea agregada")
-
-print("segunda linea agregada")
-
-print("agrego una linea adicional 13 en exporacion")
-
-print("Agrego otra linea adicional 15")
