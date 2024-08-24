48103270b4fa3e1ab6e584b68307b35fb6494b66
Javier <javier.diosesz@gmail.com>
2024-08-23 21:23:49 -0500

    actualizadas las librerias de la rama exploracion

--- exploracionDx.py
+++ exploracionDx.py
@@ -4,3 +4,9 @@ def correlacionPares():
 
 def correlacionImpares():
     return "funcion exploracion 2"
+
+print("Linea agregada")
+
+print("segunda linea agregada")
+
+print("agrego una linea adicional 13 en exporacion")
--- tratamientoDx.py
+++ tratamientoDx.py
@@ -3,3 +3,9 @@ def eliminarDx():
 
 def eliminarDx2():
     return "funcion eliminar d2 erroneos"
+
+print("Linea agregada")
+
+print("segunda linea agregada")
+
+print("agrego una linea adicional 13 en exporacion")
